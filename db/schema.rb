00958827bf5f2de925849ec77ec4c1f3b8835cfb
Livio <liviozanardo@gmail.com>
2017-07-02 21:24:07 -0400

    implemented simple_form and added route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170702170518) do
+ActiveRecord::Schema.define(version: 20170703002419) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
