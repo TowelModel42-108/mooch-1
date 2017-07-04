a668476c92a941643597ccc24f34b5a94162b718
Cece Claridge <cece.claridge@gmail.com>
2017-07-04 13:07:05 -0400

    jquery fix #2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-= require rails-ujs
-= require turbolinks
-
-= require jquery
-= require_tree .
+// = require rails-ujs
+// = require turbolinks
+//
+// = require jquery
+// = require_tree .
 
 
 function myFunction() {
